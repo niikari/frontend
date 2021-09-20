039f95d9a20515957350b45dca8905fec4f718ea
niikari <pakniiles@gmail.com>
2021-09-20 13:26:17 +0300

    Excercise 18 (Ag-grid)

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
-import TodoTable from './components/TodoTable';
 import React, { useState } from 'react';
+import Todos from './components/Todos';
 
 function App() {
 
-  const [todo, setTodo] = useState({date: '', description: ''});
-  const [todos, setTodos] = useState([]);
-
-  const inputChanged = (e) => {
-    setTodo({...todo, [e.target.name]: e.target.value})
-  }
-
-  const deleteTodo = (i) => {
-    setTodos(todos.filter((item, index) => index !== i))
-  }
-
+  
   return (
     <div className="App">
-        <h1>Add todo</h1>
-        <label>Description: </label><input name="description" value={todo.description} onChange={inputChanged}></input> 
-        <label>Date: </label><input name="date" type= "date" value={todo.date} onChange={inputChanged}></input>     
-        <button onClick={() => setTodos([todo, ...todos])}>Add</button> 
-        <TodoTable todos={todos} deleteFunction={deleteTodo}></TodoTable>
+        <Todos />
     </div>
   );
 }
--- helloreact/src/components/Todos.js
+++ helloreact/src/components/Todos.js
@@ -1,8 +1,12 @@
 import React, {useState} from "react";
+import {AgGridReact} from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function Todos() {
 
-    const [todo, setTodo] = useState({date: '', description: ''});
+    const [todo, setTodo] = useState({date: '', description: '', priority: ''});
     const [todos, setTodos] = useState([]);
 
     const inputChanged = (e) => {
@@ -14,21 +18,29 @@ function Todos() {
         setTodos(todos.filter((item, index) => index !== i));
     }
 
+    const columns = [
+        {field: 'description', sortable: true, filter:true, floatingFilter:true},
+        {field: 'date', sortable: true, filter:true, floatingFilter:true},
+        {field: 'priority', sortable: true, filter:true, floatingFilter:true, cellStyle: params => params.value === "High" ? {color:'red'} : {color:'black'}}
+    ]
+
     return (
         <>
             <h1>Add Todos</h1>
             <label>Description: </label><input name="description" value={todo.description} onChange={inputChanged}></input> 
-            <label>Date: </label><input name="date" type= "date" value={todo.date} onChange={inputChanged}></input>     
+            <label>Date: </label><input name="date" type= "date" value={todo.date} onChange={inputChanged}></input>   
+            <label>Priority: </label><input name="priority" type= "text" value={todo.priority} onChange={inputChanged}></input> 
             <button onClick={() => setTodos([todo, ...todos])}>Add</button>  
 
-            <table className="Todos">
-                <tbody>
-                    <tr><th>Date</th><th>Description</th></tr>   
-                    {
-                        todos.map((what, index) => <tr key={index}><td>{what.date}</td><td>{what.description}</td><td><button onClick={() => deleteTodo(index)}>Delete</button></td></tr>)
-                    } 
-                </tbody>    
-            </table>     
+            <div className="ag-theme-material" style={{height: 400, width: 600, margin: 'auto'}}>
+                <AgGridReact
+                    rowData={todos}
+                    columnDefs = {columns}
+                    animateRows {...true}>
+                </AgGridReact>
+
+            </div>
+               
         </>
     );
 }
