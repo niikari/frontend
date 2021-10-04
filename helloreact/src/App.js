b3b25b7f60c5ffae6296f8af06cc850579eb0a44
niikari <pakniiles@gmail.com>
2021-10-04 13:42:03 +0300

    excercise 21 (Menu)

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -4,20 +4,27 @@ import Todos from './components/Todos';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 function App() {
 
+  const [value, setValue] = useState('one')
   
+  const handleChange = (event, value) => {
+    setValue(value)
+  }
+
   return (
     <div className="App">
         <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">
-              My Todos
-            </Typography>
-          </Toolbar>
+          <Tabs value={value} onChange={handleChange}>
+            <Tab value="one" label="Home"></Tab>
+            <Tab value="two" label="My Todos"></Tab>
+          </Tabs>
         </AppBar>
-        <Todos />
+        {value === 'one' && <h1>Homepage</h1>}
+        {value === 'two' && <Todos />}
     </div>
   );
 }
